1441aeba54dccd2485af5143abeeede99739a1e7
paulsalary <sueldopablod@gmail.com>
2022-08-15 17:17:58 -0300

    back end subido

---
+++ BackEnd/src/test/java/com/portfolio/pds/PdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.pds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
